c2eabeeeaeafcaaf8e6e047f2b54367744d9763c
DineshS <dineshsadhwani4@gmail.com>
2020-09-11 21:20:32 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    // MONGO_URI: "mongodb+srv://DDS:test123@cluster0.dqnj2.mongodb.net/<dbname>?retryWrites=true&w=majority"
-
-    MONGO_URI: "mongodb+srv://DDS:test123@cluster0.dqnj2.mongodb.net/<dbname>?retryWrites=true&w=majority"
-  }
-}
